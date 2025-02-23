7238b5e52bcf2179b5959b6d596913e7ca0bd73d
Jafet1030 <eibarra24@alumnos.uaq.mx>
2025-02-22 19:10:27 -0600

    Integracion CRUD employees

---
+++ src/employees/employees.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@nestjs/common';
+import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { createECDH } from 'crypto';
+import e from 'express';
+
+@Injectable()
+export class EmployeesService {
+  private employees:CreateEmployeeDto[] = [{
+    id: 1,
+    name: "Alberto",
+    lastname: "Costas",
+    phoneNumber: "1234567890"
+  },{
+    id: 2,
+    name: "Jose",
+    lastname: "Perez",
+    phoneNumber: "0987654321"
+  }];
+  create(createEmployeeDto: CreateEmployeeDto) {
+    createEmployeeDto.id = this.employees.length + 1;
+    this.employees.push(createEmployeeDto);
+    return createEmployeeDto;
+  }
+
+  findAll() {
+    return this.employees;
+  }
+
+  findOne(id: number) {
+    const employee = this.employees.filter((employee) => employee.id === id)[0];
+    return employee;
+  }
+
+  update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
+    let employeeToUpdate= this.findOne(id);
+    employeeToUpdate= {
+      ...employeeToUpdate, 
+      ...updateEmployeeDto
+    }
+    this.employees= this.employees.map((employee) => {
+      if(employee.id === id) {
+        employee= employeeToUpdate;
+      }
+    return employee;
+    });
+    return employeeToUpdate;
+}
+
+  remove(id: number) {
+    this.employees= this.employees.filter((employee) => employee.id !== id);
+    return this.employees;
+  }
+}
