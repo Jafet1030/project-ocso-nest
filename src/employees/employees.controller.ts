a96658412e955f32e7210836a3928a09951520a2
Jafet1030 <eibarra24@alumnos.uaq.mx>
2025-03-08 22:43:49 -0600

    Auth for routes

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -3,15 +3,20 @@ import { EmployeesService } from './employees.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ROLES } from 'src/auth/constants/roles.constants';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
+  @Auth(ROLES.MANAGER)
   @Post()
   create(@Body() createEmployeeDto: CreateEmployeeDto) {
     return this.employeesService.create(createEmployeeDto);
   }
+
+  @Auth(ROLES.MANAGER, ROLES.EMPLOYEE)
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   uploadPhoto(@UploadedFile() file: Express.Multer.File){
@@ -23,16 +28,19 @@ export class EmployeesController {
     return this.employeesService.findAll();
   }
 
+  @Auth(ROLES.MANAGER)
   @Get('/:id')
   findOne(@Param('id', new ParseUUIDPipe({version:'4'})) id: string) {
     return this.employeesService.findOne(id);
   }
 
+  @Auth(ROLES.EMPLOYEE)
   @Patch(':id')
   update(@Param('id', new ParseUUIDPipe({version:'4'})) id: string, @Body() updateEmployeeDto: UpdateEmployeeDto) {
     return this.employeesService.update(id, updateEmployeeDto);
   }
 
+  @Auth(ROLES.MANAGER)
   @Delete(':id')
   remove(@Param('id', new ParseUUIDPipe({version:'4'})) id: string) {
     return this.employeesService.remove(id);
